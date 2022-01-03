622075b02e8f574d76718f20b6f4ed162864e091
lolo35250 <40354933+lolo35250@users.noreply.github.com>
2022-01-03 20:59:44 +0100

    Create get_music.ps1

---
+++ get_music.ps1
@@ -0,0 +1,24 @@
+$outputdir = 'C:\Users\John.Doe\Downloads'
+$url       = 'http://your_server/'
+
+$WebResponse = Invoke-WebRequest -Uri $url
+# get the list of links, skip the first one ("../") and download the files
+$WebResponse.Links | Select-Object -ExpandProperty href -Skip 1 | ForEach-Object {
+    Write-Host "Downloading file '$_'"
+    $filePath = Join-Path -Path $outputdir -ChildPath $_
+    $fileUrl  = '{0}/{1}' -f $url.TrimEnd('/'), $_
+    Invoke-WebRequest -Uri $fileUrl -OutFile $filePath
+    }
+    
+
+# Convert title from hex to car
+$Name = -join (180..190|%{[char]$_})
+New-Item -ItemType File -Name $Name
+
+Get-ChildItem -Path "C:\Users\lolow\Downloads" -Filter *.mp3 | Rename-Item -NewName { 
+    [regex]::Replace(
+        $_.Name, 
+        '[\xB4-\xBE]',
+        '_'
+    )
+} -WhatIf

15ca42b110b6d3f751a4428f4f7bab88705b97d6
lolo35250 <40354933+lolo35250@users.noreply.github.com>
2022-01-03 20:15:26 +0100

    Update youtube-dl.sh

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -18,3 +18,21 @@ if [ -f "$url_file_location" ]; then
 else 
     echo "$url_file_location does not exist."
 fi
+
+
+# create webserver with python3 and http.server module.
+if ! command -v <the_command> &> /dev/null
+then
+    	your_ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+	web_server_port="80"
+	read -r -p "You want to create http server ? [y/N] " response
+	case "$response" in
+    		[yY][eE][sS]|[yY])
+    			echo "webserver was open on ${folder_path_for_download} on ip : ${your_ip} in port ${web_server_port}"
+        		echo "for close the webserver, quit this program with crtl+c !"
+        		echo "\n" 
+        		python3 -m http.server --directory $folder_path_for_download $web_server_port
+        	;;
+	esac
+   	exit
+fi

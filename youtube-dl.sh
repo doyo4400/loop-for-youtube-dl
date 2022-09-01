5f925ed0c3251298751206dede3954bf6f6ba3d9
doyo4400 <40354933+doyo4400@users.noreply.github.com>
2022-09-01 08:52:13 +0200

    Update youtube-dl.sh
    Add package check / env variable

--- youtube-dl.sh
+++ youtube-dl.sh
@@ -1,38 +1,37 @@
-url_file_location="/path/of/url.txt"
-folder_path_for_download="/path/of/download/folder/"
+#verify if youtube-dl is installed
+REQUIRED_PKG="youtube-dl"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
 
-if [ -f "$url_file_location" ]; then
-	#create download folder if is doesn't exist
-	if [ ! -d "$folder_path_for_download" ]; then
-	  mkdir $folder_path_for_download
-	fi
+else
 
-	cat $url_file_location | while read line
-	do
-	  # concatenate directory
-	  directory="${folder_path_for_download}%(title)s.%(ext)s"
-	  #echo $directory
-	  youtube-dl --extract-audio --audio-format mp3 $line -o $directory
-	done
+#define directory
+direction=$(pwd)
+url_file_location="${direction}/url.txt"
+folder_path_for_download="${direction}/music_dl_from_script/"
 
-else 
-    echo "$url_file_location does not exist."
-fi
+mkdir $folder_path_for_download
+
+  if [ -f "$url_file_location" ]; then
+      #create download folder if is doesn't exist
+      if [ ! -d "$folder_path_for_download" ]; then
+        mkdir $folder_path_for_download
+      fi
+
+      cat $url_file_location | while read line
+      do
+        # concatenate directory
+        directory="${folder_path_for_download}%(title)s.%(ext)s"
+        #echo $directory
+        youtube-dl --extract-audio --audio-format mp3 $line -o $directory
+      done
+
+  else
+      echo "$url_file_location does not exist."
+  fi
 
 
-# create webserver with python3 and http.server module.
-if ! command -v <the_command> &> /dev/null
-then
-    	your_ip=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-	web_server_port="80"
-	read -r -p "You want to create http server ? [y/N] " response
-	case "$response" in
-    		[yY][eE][sS]|[yY])
-    			echo "webserver was open on ${folder_path_for_download} on ip : ${your_ip} in port ${web_server_port}"
-        		echo "for close the webserver, quit this program with crtl+c !"
-        		echo "\n" 
-        		python3 -m http.server --directory $folder_path_for_download $web_server_port
-        	;;
-	esac
-   	exit
 fi
